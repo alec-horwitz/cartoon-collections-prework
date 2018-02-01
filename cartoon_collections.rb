40d558cb5491472ae91a9fc301ed55f52a825835
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-01 15:41:31 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -20,9 +20,10 @@ end
 def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  i = 0
+  i = -1
   found = false
   while (i < cheese_types.length) || (found == true)
+    i += 1
     if array.include? cheese_types[i]
       found = true
       array.find do |item|
